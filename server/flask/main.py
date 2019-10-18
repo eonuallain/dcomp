be2baf76d6b244e2b52c1fa680a3a60c7987e734
Eamonn O Nuallain <eamonn.o.nuallain>
2019-10-18 23:50:13 +0100

    add /tasks route

--- server/flask/main.py
+++ server/flask/main.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+
+def build_task_list():
+	# will be read from DB
+	tasks = {
+		"Encryption task" : "A dummy encryption method using ROT13",
+		"Cohen-Sutherland" : "Cohen-Sutherland algorithm"
+	}
+	return tasks
 
 @app.route("/")
 def index():
 	return "dcomp"
+
+@app.route('/tasks')
+def tasks():
+    tasks = build_task_list()
+    return jsonify(tasks)
 
 if __name__ == "__main__":
     app.run()
