6b5d7801a128e5f4bc4c573abc85473fb86a6743
Eamonn O Nuallain <eamonn.o.nuallain>
2019-10-19 19:26:19 +0100

    read tasks json and display in list control

--- client/wx/dcomp-wx.py
+++ client/wx/dcomp-wx.py
@@ -1,3 +1,5 @@
+import json
+import urllib.request
 import wx
 
 class TaskPanel(wx.Panel):    
@@ -13,13 +15,25 @@ class TaskPanel(wx.Panel):
         self.list_ctrl.InsertColumn(0, 'Task', width=140)
         self.list_ctrl.InsertColumn(1, 'Description', width=300)
         main_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
-        edit_button = wx.Button(self, label='Load Tasks')
-        edit_button.Bind(wx.EVT_BUTTON, self.on_click)
-        main_sizer.Add(edit_button, 0, wx.ALL | wx.CENTER, 5)        
+        task_button = wx.Button(self, label='Load Tasks')
+        task_button.Bind(wx.EVT_BUTTON, self.on_click)
+        main_sizer.Add(task_button, 0, wx.ALL | wx.CENTER, 5)        
         self.SetSizer(main_sizer)
 
     def on_click(self, event):
         print(event)
+
+        with urllib.request.urlopen("http://localhost:5000/tasks") as url:
+            data = json.loads(url.read().decode())
+            print(data)
+
+        index = 0
+        for name, desc in data.items():
+        	print(name)
+        	print(desc)
+        	self.list_ctrl.InsertItem(index, name)
+        	self.list_ctrl.SetItem(index, 1, desc)
+        	index = index + 1
 
 class DCompFrame(wx.Frame):    
     def __init__(self):
