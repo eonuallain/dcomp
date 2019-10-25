be696b770198dda87f1e856433bf155a83c056fa
Eamonn O Nuallain <eamonn.o.nuallain>
2019-10-25 18:01:15 +0100

    flask app logger

--- server/flask/main.py
+++ server/flask/main.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify
+
+import logging
+import logging.config
 import mysql.connector
+import yaml
+
+
+with open('log.yaml', 'r') as f:
+    config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
 
 app = Flask(__name__)
- 
+log = logging.getLogger(__name__)
+
 conn = mysql.connector.connect(host="localhost", user="root", passwd="", database="dcomp")
+log.debug("connect to mysql {}".format(conn))
 cursor = conn.cursor(dictionary=True)
 
 def build_task_list():
@@ -25,6 +36,11 @@ def index():
 def tasks():
 	tasks = build_task_list()
 	return jsonify(tasks)
+
+
+@app.route('/tasks/encryption/next')
+def get_next_task_payload():
+	log.debug("route /tasks/encryption/next")
 
 if __name__ == "__main__":
 	app.run()
