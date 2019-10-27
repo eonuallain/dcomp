5f5e429152f6b262d7e9ba2c9d7441cb47b5651e
Eamonn O Nuallain <eamonn.o.nuallain>
2019-10-27 11:48:25 +0000

    add status bar with pub/sub

--- client/wx/dcomp-wx.py
+++ client/wx/dcomp-wx.py
@@ -2,6 +2,9 @@ import json
 import urllib.request
 import wx
 
+from wx.lib.pubsub import setuparg1
+from wx.lib.pubsub import pub
+
 class TaskPanel(wx.Panel):    
     def __init__(self, parent):
         super().__init__(parent)
@@ -15,14 +18,19 @@ class TaskPanel(wx.Panel):
 
         self.list_ctrl.InsertColumn(0, 'Task', width=140)
         self.list_ctrl.InsertColumn(1, 'Description', width=300)
+        self.list_ctrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_task_item_click)
+
         main_sizer.Add(self.list_ctrl, 1, wx.ALL | wx.EXPAND, 5)        
         task_button = wx.Button(self, label='Load Tasks')
-        task_button.Bind(wx.EVT_BUTTON, self.on_click)
+        task_button.Bind(wx.EVT_BUTTON, self.on_load_tasks_click)
         main_sizer.Add(task_button, 0, wx.BOTTOM | wx.CENTER, 5)
 
         self.SetSizer(main_sizer)
 
-    def on_click(self, event):
+    def on_task_item_click(self, event):
+        print(event)
+
+    def on_load_tasks_click(self, event):
         print(event)
         self.list_ctrl.DeleteAllItems()
 
@@ -36,6 +44,7 @@ class TaskPanel(wx.Panel):
                 	self.list_ctrl.InsertItem(index, name)
                 	self.list_ctrl.SetItem(index, 1, desc)
                 	index = index + 1
+            pub.sendMessage(('change_statusbar'), "Connected")
         except urllib.error.HTTPError as e:
             wx.MessageBox(str(e.code), 'Error', wx.OK | wx.ICON_ERROR)
         except urllib.error.URLError as e:
@@ -46,9 +55,16 @@ class DCompFrame(wx.Frame):
         super().__init__(parent=None,
                          title='dcomp')
         self.panel = TaskPanel(self)
+        self.statusbar = self.CreateStatusBar(1)
+        pub.subscribe(self.change_statusbar, 'change_statusbar')
+        pub.sendMessage(('change_statusbar'), "Not Connected")
         self.SetMinSize((500,250))
         self.Center()
         self.Show()
+
+    def change_statusbar(self, msg):
+        print(str(msg))
+        self.SetStatusText(msg.data)
 
 if __name__ == '__main__':
     app = wx.App(False)
