ba5110431d7778b9c229d261d0f334f7d28488e3
Eamonn O Nuallain <eamonn.o.nuallain@sap.com>
2019-10-27 21:08:40 +0000

    add stop tasks

--- client/wx/dcomp-wx.py
+++ client/wx/dcomp-wx.py
@@ -5,10 +5,14 @@ import wx
 from wx.lib.pubsub import setuparg1
 from wx.lib.pubsub import pub
 
-class TaskPanel(wx.Panel):    
+from task_enc import TaskEncryption
+
+class TaskPanel(wx.Panel):  
     def __init__(self, parent):
         super().__init__(parent)
         main_sizer = wx.BoxSizer(wx.VERTICAL)
+        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
         self.row_obj_dict = {}
 
         self.list_ctrl = wx.ListCtrl(
@@ -22,28 +26,43 @@ class TaskPanel(wx.Panel):
 
         main_sizer.Add(self.list_ctrl, 1, wx.ALL | wx.EXPAND, 5)        
         task_button = wx.Button(self, label='Load Tasks')
+        task_button.SetDefault()
         task_button.Bind(wx.EVT_BUTTON, self.on_load_tasks_click)
         main_sizer.Add(task_button, 0, wx.BOTTOM | wx.CENTER, 5)
+        button_sizer.Add(task_button, 0, wx.CENTER|wx.ALL, 5)
+
+        stop_button = wx.Button(self, label='Stop Tasks')
+        stop_button.Disable()
+        button_sizer.Add(stop_button, 0, wx.CENTER|wx.ALL, 5)
+        main_sizer.Add(button_sizer, 0, wx.CENTER)
 
         self.SetSizer(main_sizer)
 
+    def start_task(self, str):
+        print("start_task called with", str, flush=True)
+        task_encryption.run()
+
     def on_task_item_click(self, event):
-        print(event)
+        index = self.list_ctrl.GetFocusedItem()
+
+        if index >= 0:
+            print("selected {}".format(str(self.row_obj_dict[index])), flush=True)
+
+            task = str(self.row_obj_dict[index])
+            self.start_task(task)
+
 
     def on_load_tasks_click(self, event):
-        print(event)
-        self.list_ctrl.DeleteAllItems()
-
         try:
             with urllib.request.urlopen("http://localhost:5000/tasks") as url:
                 data = json.loads(url.read().decode())
-                print(data)
 
                 index = 0
                 for name, desc in data.items():
-                	self.list_ctrl.InsertItem(index, name)
-                	self.list_ctrl.SetItem(index, 1, desc)
-                	index = index + 1
+                    self.list_ctrl.InsertItem(index, name)
+                    self.list_ctrl.SetItem(index, 1, desc)
+                    self.row_obj_dict[index] = name
+                    index = index + 1
             pub.sendMessage(('change_statusbar_main'), "Connected")
         except urllib.error.HTTPError as e:
             wx.MessageBox(str(e.code), 'Error', wx.OK | wx.ICON_ERROR)
@@ -72,8 +91,13 @@ class DCompFrame(wx.Frame):
         self.SetStatusText(msg.data, 1)
 
 if __name__ == '__main__':
+
     app = wx.App(False)
     frame = DCompFrame()
     #icon from http://icons8.com/
     frame.SetIcon(wx.Icon("Sirubico-Black-Metal-PC.ico"))
+
+    task_encryption = TaskEncryption()
+
+    print(str(task_encryption), flush=True)
     app.MainLoop()
