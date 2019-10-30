312cb37b1e4c0ef17b301a19092f9b2c069c7559
Eamonn O Nuallain <eamonn.o.nuallain>
2019-10-30 00:03:35 +0000

    fetch payload

--- client/console/task_enc.py
+++ client/console/task_enc.py
@@ -1,7 +1,23 @@
+import json
+import time
+import urllib.request
+
 from task_base import TaskBase
 
 class TaskEncryption(TaskBase):
 
-    def run(self):
-      print(self)
+	def run(self):
+		print(self)
+
+		for x in range(0, 2000):
+			self.get_plain_text()
+			#don't kill the server
+			print("sleeping for 2 seconds ...")
+			time.sleep(2)
+
+	def get_plain_text(self):
+		url_enc = "http://localhost:5000/task/encryption/next"
+		with urllib.request.urlopen(url_enc) as url:
+			data = json.loads(url.read().decode())
+			print(data)
 
