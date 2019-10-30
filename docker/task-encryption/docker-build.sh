932c89120f5a7a4e75d39bef09ab5070c20878bd
Eamonn O Nuallain <eamonn.o.nuallain>
2019-10-30 22:25:03 +0000

    fix images tagging

--- docker/task-encryption/docker-build.sh
+++ docker/task-encryption/docker-build.sh
@@ -3,4 +3,14 @@
 cp ../../client/console/task_base.py .
 cp ../../client/console/task_enc.py .
 
-docker build .
+TAG=v20191030_1
+REPO=docker.io
+BASE=eonuallain/dcomp-task-enc
+FULL=$REPO/$BASE
+
+echo "tagging as $FULL:$TAG"
+
+ID=$(docker build -t $FULL:$TAG -t $FULL:latest .)
+
+docker push $FULL:$TAG
+docker push $FULL:latest
